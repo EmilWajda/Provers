15c32ccbf371bb7ed74fcfdf759165c1421938f3
Atras19 <emil.wajda.ew@gmail.com>
2025-11-20 20:17:26 +0100

    add an example generating a formula

--- core/loft/__main__.py
+++ core/loft/__main__.py
@@ -1,1 +1,16 @@
-print("Hello world")
+from .formulas  import ForAll, Exists, Not, Alternative, Implication, GreaterThan, Atom, Conjunction, BasicToken, LogicToken
+from .generators.generator import Generator
+from .generators.problem1 import Problem1
+
+
+# p = ForAll("U", Exists("V", Conjunction([GreaterThan("U", "V"), Implication(Not(Atom("var14", "V")), Atom("var5", "U"))])))
+
+# print(p.to_tptp())
+
+p = Problem1(142, {"clauses": 10, "lengths": [2,3,4]})
+clauses = p.generate()
+
+for clause in clauses:
+    print(clause.to_tptp())
+    print()
+    print()
