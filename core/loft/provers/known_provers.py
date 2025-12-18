d13ad89c34de59b65e6dfe0ded1da53259e239da
Kacper0510 <kacper.b.wojciuch@gmail.com>
2025-12-18 02:00:44 +0100

    Add eprover

--- core/loft/provers/known_provers.py
+++ core/loft/provers/known_provers.py
@@ -4,4 +4,5 @@ from .run_output import basic_result_parser
 KNOWN_PROVERS: dict[str, Prover] = {  # TODO: verify result parsers
     "vampire": Prover("vampire", basic_result_parser("satisfiable", "unsatisfiable")),
     "spass": Prover("spass", basic_result_parser("completion found", "proof found")),
+    "e": Prover("e", basic_result_parser("satisfiable", "unsatisfiable")),
 }
