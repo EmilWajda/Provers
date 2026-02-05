c58a957b7921582af26510842d518f995443c825
Kacper0510 <kacper.b.wojciuch@gmail.com>
2026-02-06 00:28:09 +0100

    Fix timeout freeze issue

--- core/loft/docker.py
+++ core/loft/docker.py
@@ -1,14 +1,18 @@
 import asyncio
 import aiofiles
+import time
 
 
 async def run_docker_container(name: str, input_file: str, timeout: int | None = None) -> tuple[str, str, int | None]:
     async with aiofiles.open(input_file, "rb") as f:
         input_data = await f.read()
+    container_name = f"loft-{time.time_ns()}"
     process = await asyncio.create_subprocess_exec(
         "docker",
         "compose",
         "run",
+        "--name",
+        container_name,
         "--rm",
         name,
         stdin=asyncio.subprocess.PIPE,
@@ -20,8 +24,10 @@ async def run_docker_container(name: str, input_file: str, timeout: int | None =
         try:
             stdout, stderr = await asyncio.wait_for(process.communicate(input=input_data), timeout=timeout)
         except asyncio.TimeoutError:
-            process.terminate()
-            stdout, stderr = await process.communicate()
+            kill_process = await asyncio.create_subprocess_exec(
+                "docker", "kill", container_name, stdout=asyncio.subprocess.DEVNULL, stderr=asyncio.subprocess.DEVNULL
+            )
+            stdout, stderr = (await asyncio.gather(process.communicate(), kill_process.wait()))[0]
             return stdout.decode(), stderr.decode(), None
     else:
         stdout, stderr = await process.communicate(input=input_data)
